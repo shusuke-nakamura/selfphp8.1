a7bbb395c17b12d524160dc88593900324e323fe
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-12-19 22:54:52 +0900

    chap01
    学習完了

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # selfphp8.1
+
+☆ php.iniのパス
+/opt/phpenv/versions/8.1.0/etc/php.ini
+
+☆ mariadbのユーザ作成
+GRANT ALL PRIVILEGES ON selfphp.* TO selfusr@"%" IDENTIFIED BY 'selfpass';
+
+☆ Xdebug
+sudo vi /opt/phpenv/versions/8.1.0/etc/conf.d/xdebug.ini
+以下の2行を末尾に追加して、apacheを再起動
+xdebug.mode=debug
+xdebug.start_with_request=yes
---
+++ chap01/hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPの基本</title>
+</head>
+
+<body>
+    <?php
+    print 'こんにちは世界！<br />';
+    print 'こんにちは、皆さん！';
+    ?>
+</body>
+
+</html>
