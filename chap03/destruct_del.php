32b940d2d3559993e951797f9c65f4553f6e41c1
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-12-31 21:22:09 +0900

    リスト3.15
    学習完了

---
+++ chap03/destruct_del.php
@@ -0,0 +1,7 @@
+<?php
+$data = [1, 2, 3, 4, 5];
+[, $a,, $b, $c] = $data;
+
+print $a;
+print $b;
+print $c;
---
+++ chap03/m_increament.php
@@ -0,0 +1,8 @@
+<?php
+$i = 'Z';
+print ++$i;
+print ++$i;
+
+$j = 'T8';
+print ++$j;
+print ++$j;
