0d2353f1617c69fdd58c5a0df52618ccb30d8d37
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-02-27 00:55:58 +0900

    リスト5.9まで学習完了

---
+++ chap05/mb_substr.php
@@ -0,0 +1,7 @@
+<?php
+$str = 'WINGSプロジェクト';
+
+print mb_substr($str, 5, 2);
+print mb_substr($str, 5);
+print mb_substr($str, 5, -4);
+print mb_substr($str, -6, 2);
---
+++ chap05/mbstrstr.php
@@ -0,0 +1,5 @@
+<?php
+$str = 'WINGSプロジェクト';
+print mb_strstr($str, 'S', true);
+print mb_strstr($str, 'S');
+print mb_strstr($str, 'M', false);
