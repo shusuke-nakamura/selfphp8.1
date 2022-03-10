6d606483c6d59689446164b7ef150d61ec708f6e
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-03-10 22:04:44 +0900

    chap05 学習完了

---
+++ chap05/math.php
@@ -0,0 +1,20 @@
+<?php
+print abs(-100);
+print base_convert(100, 2, 10);
+print ceil(1234.56);
+print floor(1234.56);
+print intdiv(10, 3);
+print fmod(10.5, 5);
+print max(1, 5, 3);
+print min(1, 5, 3);
+print rand(5, 10);
+print pow(2, 4);
+print round(1234.56, 2);
+print sqrt(10000);
+print cos(deg2rad(60));
+print sin(deg2rad(30));
+print tan(deg2rad(45));
+print deg2rad(180);
+print exp(1);
+print log10(100);
+print log(125, 5);
