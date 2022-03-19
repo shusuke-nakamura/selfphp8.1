85bcbe4503c407f77b52acc04a633b4bdc0935b1
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-03-20 01:15:26 +0900

    リスト7.8まで学習完了

---
+++ chap07/datetime_tz.php
@@ -0,0 +1,7 @@
+<?php
+$dt1 = new DateTime('now', new DateTimeZone('Asia/Ulan_Bator'));
+print $dt1->format('Y年m月d日 H時i分');
+$dt2 = new DateTime('now', new DateTimeZone('America/Virgin'));
+print $dt2->format('Y年m月d日 H時i分');
+$dt3 = new DateTime('now', new DateTimeZone('Europe/London'));
+print $dt3->format('Y年m月d日 H時i分');
