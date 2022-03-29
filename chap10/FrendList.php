4ad2d69af813a4d43a35f2d34e83aea84d6bd22a
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-03-29 19:37:01 +0900

    10.6 オブジェクトの操作の学習完了

---
+++ chap10/FrendList.php
@@ -0,0 +1,18 @@
+<?php
+class FrendList implements IteratorAggregate
+{
+
+    public string $version = '1.0.0';
+    public string $name = '友人リスト';
+    private array $list = [];
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->list);
+    }
+
+    public function add(Person $p): void
+    {
+        $this->list[] = $p;
+    }
+}
--- chap10/trait_alias.php
+++ chap10/trait_alias.php
@@ -17,8 +17,12 @@ trait MyTrait2
 
 class MyClass
 {
-    use MyTrait1, MyTrait2;
+    use MyTrait1, MyTrait2 {
+        MyTrait1::hoge insteadof MyTrait2;
+        MyTrait2::hoge as foo;
+    }
 }
 
 $cls = new MyClass();
 $cls->hoge();
+$cls->foo();
