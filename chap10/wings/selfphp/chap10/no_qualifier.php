874236ce99bba92e2bbb08260d1870b208fce265
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-04-02 19:36:11 +0900

    10.9 名前空間の学習完了

---
+++ chap10/wings/selfphp/chap10/no_qualifier.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace wings\selfphp\chap10;
+
+require_once 'MyClass.php';
+print MyClass::showClass();
---
+++ chap10/wings/selfphp/qualifier.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace wings\selfphp;
+
+require_once 'chap10/MyClass.php';
+print chap10\MyClass::showClass();
